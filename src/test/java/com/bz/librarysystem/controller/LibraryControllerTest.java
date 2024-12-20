3f9c5e43c64d48d81f021f202c08fc28fcae6071
bzaman18 <36867289+bzaman18@users.noreply.github.com>
2024-12-20 16:42:55 +0000

    fixed typo with class file name

--- src/test/java/com/bz/librarysystem/controller/LibraryControllerTest.java
+++ src/test/java/com/bz/librarysystem/controller/LibraryControllerTest.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
-class BookControllerTest {
+class LibraryControllerTest {
 
     @Mock
     private LibraryService libraryService;
